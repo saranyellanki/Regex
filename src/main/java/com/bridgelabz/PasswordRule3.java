c741559832fedef146d7b2e27e2fc919d2a54930
saranyellanki <saran.yellanki12@gmail.com>
2021-11-09 19:21:28 +0530

    Added: Password with atleast 1 numeric number

---
+++ src/main/java/com/bridgelabz/PasswordRule3.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordRule3 {
+    /**
+     * This method takes password with minimum of 8 chars
+     * and has at least 1 numeric number
+     * @param password is used for comparing with regex
+     * @return boolean true if matches else false
+     */
+    public boolean isPassword(String password) {
+        String regex = "^(?=.*[0-9])[a-zA-Z0-9+-_!@#$%^&*(){}'.,]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PasswordRule3 pass = new PasswordRule3();
+        System.out.println(pass.isPassword("nfgo#2@4Yr"));
+        System.out.println(pass.isPassword("eiunegtce"));
+    }
+}
---
+++ src/test/java/com/bridgelabz/TestPasswordRule3.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPasswordRule3 {
+    PasswordRule3 password;
+    @Before
+    public void initializeTest(){
+        password = new PasswordRule3();
+    }
+    @Test
+    public void givenPasswordWhenAddedShouldReturnBoolean(){
+        boolean checkPass = password.isPassword("Bgdoh@nwe");
+        Assert.assertFalse(checkPass);
+        boolean checkPass1 = password.isPassword("kidv6h#4ef");
+        Assert.assertTrue(checkPass1);
+    }
+}
